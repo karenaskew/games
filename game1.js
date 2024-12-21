0c3e4d1ef135fc880f24b8e2072670edf7d9e946
karenaskew <askewk8@googlemail.com>
2024-12-21 13:04:34 +1000

    insert game name

--- game1.js
+++ game1.js
@@ -1,8 +1,8 @@
 
 document.getElementById("button1").addEventListener("click", rollSet1);
 function rollSet1() {
-    var diceFirst = document.getElementById("firstDice")
-    var diceSecond = document.getElementById("secondDice")
+    var diceFirst = document.getElementById("firstDice");
+    var diceSecond = document.getElementById("secondDice");
    
     var randomNum1 = Math.floor(Math.random() * 6)+1;
     var randomNum2 = Math.floor(Math.random() * 6)+1;
@@ -14,11 +14,34 @@ function rollSet1() {
     diceFirst.setAttribute("alt", randomNum1);
     diceSecond.setAttribute("src", diceRandom2);
     diceSecond.setAttribute("alt", randomNum2);
+
+    // sum dice
+    var diceSum = num1 + num2;
+    document.getElementById("set1Sum").setAttribute("innerHtml", diceSum)
+
+    let gameDictionary = {};
+    gameDictionary[2] = "You";
+    gameDictionary[3] = "Me";
+    gameDictionary[4] = "Girls";
+    gameDictionary[5] = "5Alive";
+    gameDictionary[6] = "Guys";
+    gameDictionary[7] = "Heaven";
+    gameDictionary[8] = "War";
+    gameDictionary[9] = "Rhyme";
+    gameDictionary[10] = "Categories";
+    gameDictionary[11] = "Truth";
+    gameDictionary[12] = "Higher/Lower";
+   
+    var gameName = gameDictionary.diceSum[0]
+    document.getElementById("set1Game").setAttribute("innerHtml", gameName)
+
 };
+
+    
 
 document.getElementById("button2").addEventListener("click", rollSet1);
 function rollSet2() {
-    var diceThird = document.getElementById("thirdDice")
+    var diceThird = document.getElementById("thirdDice");
     
     var randomNum3 = Math.floor(Math.random() * 6)+1;
 
@@ -28,3 +51,4 @@ function rollSet2() {
     diceThird.setAttribute("alt", randomNum3);
 
 };
+
